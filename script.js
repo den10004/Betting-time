c489e9acc691699260780d6fc598130ac9b15f2f
Denis Sergeev <denis-sergeev-85@inbox.ru>
2022-04-13 17:13:02 +0300

    начата 3-я страница

--- script.js
+++ script.js
@@ -38,3 +38,51 @@ var swiper = new Swiper(".expertsSwiper", {
     },
   },
 });
+
+var swiper = new Swiper(".expertsForecast", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: false,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-expert",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 5.5,
+    },
+    375: {
+      slidesPerView: 6,
+    },
+    1024: {
+      slidesPerView: 8,
+    },
+  },
+});
+
+var swiper = new Swiper(".tabsForecast1", {
+  loop: true,
+  slidesPerView: "auto",
+  centeredSlides: false,
+  spaceBetween: 5,
+  navigation: {
+    nextEl: ".swiper-button-expert",
+  },
+  breakpoints: {
+    320: {
+      slidesPerView: 2.5,
+    },
+    375: {
+      slidesPerView: 2.5,
+    },
+    425: {
+      slidesPerView: 3,
+    },
+    768: {
+      slidesPerView: 4,
+    },
+    1024: {
+      slidesPerView: 4,
+    },
+  },
+});
